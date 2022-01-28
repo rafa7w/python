dca425cb325e06b4e3d89ca3e5762d59863fe8dd
Rafael William <rafael.fim@hotmail.com>
2022-01-28 09:27:08 -0300

    connecting GUI widgets with functions

--- tkinter/script.py
+++ tkinter/script.py
@@ -2,14 +2,19 @@ from tkinter import *
 
 window=Tk()
 
+def km_to_miles():
+  miles=float(e1_value.get()) * 1.6
+  t1.insert(END, miles)
+
 # Para acessar informações sobre os widgets, basta chamar no cmd ipython, fazer a importação do tkinter (from tkinter import *) e rodar, por exemplo, Button?
-b1=Button(window, text='Execute')
+b1=Button(window, text='Execute', command=km_to_miles)
 b1.grid(row=0, column=0)
 
-e1=Entry(window)
+e1_value=StringVar()
+e1=Entry(window, textvariable=e1_value)
 e1.grid(row=0, column=1)
 
-t1=Text(window, height=5, width=30)
+t1=Text(window, height=1, width=30)
 t1.grid(row=0, column=2)
 
 
