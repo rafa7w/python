6b78e9241ab27dac80ea1f647f0e5cbedc128573
Rafael William <rafael.fim@hotmail.com>
2022-01-28 10:32:52 -0300

    update and delete methods

--- database/sqlite/methods.py
+++ database/sqlite/methods.py
@@ -8,12 +8,30 @@ def create_table():
   conn.commit()
   conn.close()
   
+
 def insert(item, qtd, price):  
   conn=sqlite3.connect('lite.db')
   cursor=conn.cursor()
   cursor.execute('INSERT INTO store VALUES (?, ?, ?)', (item, qtd, price))
   conn.commit()
   conn.close()
+
+
+def update(item, qtd, price):  
+  conn=sqlite3.connect('lite.db')
+  cursor=conn.cursor()
+  cursor.execute('UPDATE store SET quantity=?, price=? WHERE item=?', (qtd, price, item))
+  conn.commit()
+  conn.close()
+
+
+def delete(item):  
+  conn=sqlite3.connect('lite.db')
+  cursor=conn.cursor()
+  cursor.execute('DELETE FROM store WHERE item=?', (item,))
+  conn.commit()
+  conn.close()
+
 
 def view():
   conn=sqlite3.connect('lite.db')
